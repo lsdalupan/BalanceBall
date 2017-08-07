3bf4b27c8713f3e7925c92b3522b187b46492acf
daluplo <DALUPLO@ZHA-ITA106-W7.corp.oocl.com>
2017-08-07 20:13:09 +0800

    add first code

---
+++ src/balanceBallSolver/BalanceBall.java
@@ -0,0 +1,17 @@
+package balanceBallSolver;
+
+public class BalanceBall {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		solver();
+	}
+	
+	public static void solver() {
+		
+	}
+
+}
+
+
+
